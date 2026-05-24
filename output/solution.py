3d85ca63e30dd2c76fe60fb418ccd52a1fa25ef9
BinduSatya <BinduSatya@users.noreply.github.com>
2026-05-24 05:05:03 +0000

    deploy: 3434263c0a7181564e1c28c62a7db26f3b941b74

--- output/solution.py
+++ output/solution.py
@@ -1,15 +1,53 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int count = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                count++;
+    int maxJumps(vector<int>& arr, int d) {
+        int n = arr.size();
+        vector<int> memo(n, -1);
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = max(result, dfs(arr, d, memo, i));
+        }
+        
+        return result;
+    }
+    
+    int dfs(vector<int>& arr, int d, vector<int>& memo, int i) {
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        
+        int maxJumps = 1;
+        for (int j = max(0, i - d); j <= min((int)arr.size() - 1, i + d); j++) {
+            if (i == j) {
+                continue;
+            }
+            
+            if (arr[i] > arr[j]) {
+                bool canJump = true;
+                if (j < i) {
+                    for (int k = j + 1; k < i; k++) {
+                        if (arr[k] >= arr[j]) {
+                            canJump = false;
+                            break;
+                        }
+                    }
+                } else {
+                    for (int k = i + 1; k < j; k++) {
+                        if (arr[k] >= arr[j]) {
+                            canJump = false;
+                            break;
+                        }
+                    }
+                }
+                
+                if (canJump) {
+                    maxJumps = max(maxJumps, 1 + dfs(arr, d, memo, j));
+                }
             }
         }
-        if (nums.back() > nums.front()) {
-            count++;
-        }
-        return count <= 1;
+        
+        memo[i] = maxJumps;
+        return maxJumps;
     }
 };
