424ec53a37c4bdf0e4f6543f13a2e7e381fb3fec
BinduSatya <BinduSatya@users.noreply.github.com>
2026-05-29 05:17:10 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
-        vector<int> result;
-        for (const auto& query : wordsQuery) {
-            int maxLen = 0, minLen = INT_MAX, index = -1;
-            for (int i = 0; i < wordsContainer.size(); i++) {
-                int commonLen = 0;
-                for (int j = query.size() - 1, k = wordsContainer[i].size() - 1; j >= 0 && k >= 0; j--, k--) {
-                    if (query[j] != wordsContainer[i][k]) break;
-                    commonLen++;
-                }
-                if (commonLen > maxLen) {
-                    maxLen = commonLen;
-                    minLen = wordsContainer[i].size();
-                    index = i;
-                } else if (commonLen == maxLen) {
-                    if (wordsContainer[i].size() < minLen) {
-                        minLen = wordsContainer[i].size();
-                        index = i;
-                    }
-                }
+    int minElement(vector<int>& nums) {
+        int min_sum = INT_MAX;
+        for (int num : nums) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
             }
-            result.push_back(index);
+            min_sum = min(min_sum, sum);
         }
-        return result;
+        return min_sum;
     }
 };
