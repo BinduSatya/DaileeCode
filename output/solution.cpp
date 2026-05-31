565b13dcd2027d4dc947a5c553ef0657d61d1de6
BinduSatya <BinduSatya@users.noreply.github.com>
2026-05-31 05:21:38 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
-    vector<bool> getResults(vector<vector<int>>& queries) {
-        vector<bool> results;
-        set<int> obstacles;
-        for (auto& query : queries) {
-            if (query[0] == 1) {
-                obstacles.insert(query[1]);
-            } else {
-                bool canPlace = true;
-                int x = query[1], sz = query[2];
-                auto it = obstacles.lower_bound(0);
-                int prev = 0;
-                while (it != obstacles.end() && *it <= x) {
-                    if (*it - prev < sz) {
-                        canPlace = false;
-                        break;
-                    }
-                    prev = *it;
-                    ++it;
-                }
-                if (canPlace && prev + sz <= x) {
-                    canPlace = true;
-                } else {
-                    canPlace = false;
-                }
-                results.push_back(canPlace);
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        for (int asteroid : asteroids) {
+            if (mass < asteroid) {
+                return false;
             }
+            mass += asteroid;
         }
-        return results;
+        return true;
     }
 };
