e399d247ec369ec1e532c6e5e8d447f521bd402a
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-01 06:06:33 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(), asteroids.end());
-        for (int asteroid : asteroids) {
-            if (mass < asteroid) {
-                return false;
+    int minimumCost(vector<int>& cost) {
+        int n = cost.size();
+        sort(cost.begin(), cost.end());
+        int totalCost = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i > 1) {
+                totalCost += cost[i] + cost[i - 1];
+                i--;
+            } else {
+                totalCost += cost[i];
             }
-            mass += asteroid;
         }
-        return true;
+        return totalCost;
     }
 };
