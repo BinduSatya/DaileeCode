48d29b172372a976e8aa8937440d99dea7c80cd1
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-02 05:51:29 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int minimumCost(vector<int>& cost) {
-        int n = cost.size();
-        sort(cost.begin(), cost.end());
-        int totalCost = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i > 1) {
-                totalCost += cost[i] + cost[i - 1];
-                i--;
-            } else {
-                totalCost += cost[i];
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int minTime = INT_MAX;
+        
+        // Land ride first
+        for (int i = 0; i < landStartTime.size(); i++) {
+            for (int j = 0; j < waterStartTime.size(); j++) {
+                int finishTime = max(landStartTime[i] + landDuration[i], waterStartTime[j]) + waterDuration[j];
+                minTime = min(minTime, finishTime);
             }
         }
-        return totalCost;
+        
+        // Water ride first
+        for (int i = 0; i < waterStartTime.size(); i++) {
+            for (int j = 0; j < landStartTime.size(); j++) {
+                int finishTime = max(waterStartTime[i] + waterDuration[i], landStartTime[j]) + landDuration[j];
+                minTime = min(minTime, finishTime);
+            }
+        }
+        
+        return minTime;
     }
 };
