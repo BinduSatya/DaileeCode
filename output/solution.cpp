bf31fe9513e124026a2542cb7252627910f085c5
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-03 06:08:30 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -3,19 +3,10 @@ public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int minTime = INT_MAX;
         
-        // Land ride first
         for (int i = 0; i < landStartTime.size(); i++) {
             for (int j = 0; j < waterStartTime.size(); j++) {
-                int finishTime = max(landStartTime[i] + landDuration[i], waterStartTime[j]) + waterDuration[j];
-                minTime = min(minTime, finishTime);
-            }
-        }
-        
-        // Water ride first
-        for (int i = 0; i < waterStartTime.size(); i++) {
-            for (int j = 0; j < landStartTime.size(); j++) {
-                int finishTime = max(waterStartTime[i] + waterDuration[i], landStartTime[j]) + landDuration[j];
-                minTime = min(minTime, finishTime);
+                minTime = min(minTime, max(landStartTime[i] + landDuration[i], waterStartTime[j]) + waterDuration[j]);
+                minTime = min(minTime, max(waterStartTime[j] + waterDuration[j], landStartTime[i]) + landDuration[i]);
             }
         }
         
