97fc2bd702ebe1958ecb3eb4414d946dc5d802e5
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-08 05:54:19 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,50 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> nodes;
-        
-        for (auto& desc : descriptions) {
-            int parent = desc[0];
-            int child = desc[1];
-            bool isLeft = desc[2];
-            
-            if (nodes.find(parent) == nodes.end()) {
-                nodes[parent] = new TreeNode(parent);
-            }
-            
-            if (nodes.find(child) == nodes.end()) {
-                nodes[child] = new TreeNode(child);
-            }
-            
-            if (isLeft) {
-                nodes[parent]->left = nodes[child];
-            } else {
-                nodes[parent]->right = nodes[child];
-            }
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> less, equal, greater;
+        for (int num : nums) {
+            if (num < pivot) less.push_back(num);
+            else if (num == pivot) equal.push_back(num);
+            else greater.push_back(num);
         }
-        
-        unordered_set<int> children;
-        for (auto& desc : descriptions) {
-            children.insert(desc[1]);
-        }
-        
-        for (const auto& node : nodes) {
-            if (children.find(node.first) == children.end()) {
-                return node.second;
-            }
-        }
-        
-        return nullptr; 
+        less.insert(less.end(), equal.begin(), equal.end());
+        less.insert(less.end(), greater.begin(), greater.end());
+        return less;
     }
 };
