626b7303f53eef5b452ec6eb401a6b74c9a4fb19
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-10 05:22:22 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int n = nums.size();
-        long long res = 0;
-        for (int l = 0; l < n; l++) {
-            int mx = nums[l], mn = nums[l];
-            for (int r = l; r < n; r++) {
-                mx = max(mx, nums[r]);
-                mn = min(mn, nums[r]);
-                res = max(res, static_cast<long long>(mx - mn) * k);
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k && j <= i * (i + 1) / 2; j++) {
+                long long maxVal = 0;
+                for (int l = 0; l < i; l++) {
+                    long long val = *max_element(nums.begin() + l, nums.begin() + i) - *min_element(nums.begin() + l, nums.begin() + i) + dp[l][j - 1];
+                    maxVal = max(maxVal, val);
+                }
+                dp[i][j] = maxVal;
             }
         }
-        return res;
+        
+        return dp[n][k];
     }
 };
