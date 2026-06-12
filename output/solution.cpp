a039d119e40687cb801f9f5adb9133819bae24ca
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-12 05:52:41 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,50 +1,44 @@
 #include <vector>
-#include <queue>
+#include <unordered_map>
+
 using namespace std;
 const int MOD = 1e9 + 7;
 
+void dfs(int u, int p, const vector<vector<int>>& edges, vector<vector<int>>& adj, vector<int>& depth) {
+    for (int v : adj[u]) if (v != p) {
+        depth[v] = depth[u] + 1;
+        dfs(v, u, edges, adj, depth);
+    }
+}
+
 class Solution {
 public:
-    int edgeScore(vector<vector<int>>& edges, int node) {
-        vector<vector<int>> graph(edges.size() + 1);
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        int n = edges.size() + 1;
+        vector<vector<int>> adj(n);
         for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+            adj[edge[0] - 1].push_back(edge[1] - 1);
+            adj[edge[1] - 1].push_back(edge[0] - 1);
         }
-        return dfs(graph, node, 1);
+        
+        vector<int> depth(n, 0);
+        dfs(0, -1, edges, adj, depth);
+        
+        vector<int> res(queries.size(), 0);
+        for (int i = 0; i < queries.size(); i++) {
+            int len = abs(depth[queries[i][0] - 1] - depth[queries[i][1] - 1]);
+            if (len & 1) res[i] = exp(2, len - 1);
+            else res[i] = exp(2, len - 1);
+        }
+        
+        return res;
     }
-
-    int dfs(vector<vector<int>>& graph, int target, int node) {
-        if (node == target) return 1;
-        int ans = 0;
-        for (int child : graph[node]) {
-            ans = (ans + dfs(graph, target, child)) % MOD;
-        }
-        ans = (ans * 2) % MOD;
-        return ans;
-    }
-
-    int assignEdgeWeights(vector<vector<int>>& edges) {
-        vector<vector<int>> graph(edges.size() + 1);
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-        }
-        queue<pair<int, int>> q;
-        q.push({1, 0});
-        int maxDepth = -1;
-        pair<int, int> deepest;
-        while (!q.empty()) {
-            auto node = q.front();
-            q.pop();
-            if (node.second > maxDepth) {
-                maxDepth = node.second;
-                deepest = node;
-            }
-            for (int child : graph[node.first]) {
-                q.push({child, node.second + 1});
-            }
-        }
-        return edgeScore(edges, deepest.first);
+    
+    long long exp(int b, int p) {
+        if (p == 0) return 1;
+        long long r = exp(b, p / 2) % MOD;
+        r = r * r % MOD;
+        if (p & 1) return r * b % MOD;
+        return r;
     }
 };
