8e1326d7ab24af76d5b714f0a9ef55330309a4c9
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-16 06:36:52 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,44 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* deleteMiddle(ListNode* head) {
-        // Handle edge cases
-        if (!head) return nullptr;
-        if (!head->next) return nullptr;
-
-        // Initialize slow and fast pointers
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        // Initialize a node to keep track of the node before the middle node
-        ListNode* prevSlow = nullptr;
-
-        // Find the middle node
-        while (fast->next && fast->next->next) {
-            prevSlow = slow;
-            slow = slow->next;
-            fast = fast->next->next;
+    string processStr(string s) {
+        string result = "";
+        for (char c : s) {
+            if (c == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            } else if (c == '#') {
+                result += result;
+            } else if (c == '%') {
+                reverse(result.begin(), result.end());
+            } else {
+                result += c;
+            }
         }
-
-        // If the middle node is the second node, then it's part of a 2-node list or the second half of an odd-length list
-        if (slow == head) {
-            return head->next;
-        } else if (slow->next) {
-            prevSlow->next = slow->next;
-        } else {
-            // If the middle node is the last node, then it's the only node or the second node in a 2-node list
-            prevSlow->next = nullptr;
-        }
-
-        return head;
+        return result;
     }
 };
