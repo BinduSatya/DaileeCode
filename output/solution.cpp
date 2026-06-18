728c635b162a787818a3174a1af8598b5d68abba
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-18 05:59:31 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    char processStr(string s, long long k) {
-        string result = "";
-        
-        for (char c : s) {
-            if (c == '*') {
-                if (!result.empty()) result.pop_back();
-            } else if (c == '#') {
-                result += result;
-            } else if (c == '%') {
-                reverse(result.begin(), result.end());
-            } else {
-                result += c;
-            }
-        }
-        
-        if (k >= result.size()) return '.';
-        return result[k];
+    double angleClock(int hour, int minutes) {
+        double hourAngle = (hour % 12 + minutes / 60.0) * 30;
+        double minuteAngle = minutes * 6;
+        double angle = abs(hourAngle - minuteAngle);
+        return min(angle, 360 - angle);
     }
 };
