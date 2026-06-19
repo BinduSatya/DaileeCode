a96d536b343cd2617efa5fdbd5a85692ba20d0df
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-19 06:23:52 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
-    double angleClock(int hour, int minutes) {
-        double hourAngle = (hour % 12 + minutes / 60.0) * 30;
-        double minuteAngle = minutes * 6;
-        double angle = abs(hourAngle - minuteAngle);
-        return min(angle, 360 - angle);
+    int largestAltitude(vector<int>& gain) {
+        int max_altitude = 0;
+        int current_altitude = 0;
+        
+        for (int i = 0; i < gain.size(); i++) {
+            current_altitude += gain[i];
+            max_altitude = max(max_altitude, current_altitude);
+        }
+        
+        return max_altitude;
     }
 };
