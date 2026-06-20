be74ed06e899dda4cad1cc76ca175ec8840be9e4
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-20 05:17:46 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int largestAltitude(vector<int>& gain) {
-        int max_altitude = 0;
-        int current_altitude = 0;
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+        restrictions.push_back({1, 0});
+        sort(restrictions.begin(), restrictions.end());
         
-        for (int i = 0; i < gain.size(); i++) {
-            current_altitude += gain[i];
-            max_altitude = max(max_altitude, current_altitude);
+        for (int i = 0; i < restrictions.size() - 1; i++) {
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0]);
         }
         
-        return max_altitude;
+        for (int i = restrictions.size() - 1; i > 0; i--) {
+            restrictions[i - 1][1] = min(restrictions[i - 1][1], restrictions[i][1] + restrictions[i][0] - restrictions[i - 1][0]);
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < restrictions.size() - 1; i++) {
+            ans = max(ans, restrictions[i][1] + (restrictions[i + 1][0] - restrictions[i][0] - 1) / 2);
+        }
+        
+        return ans;
     }
 };
