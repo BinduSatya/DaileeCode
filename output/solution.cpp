4b6b04173294c4f2d6c51a8905f7d737e84f6cef
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-21 06:05:08 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int maxBuilding(int n, vector<vector<int>>& restrictions) {
-        restrictions.push_back({1, 0});
-        sort(restrictions.begin(), restrictions.end());
-        
-        for (int i = 0; i < restrictions.size() - 1; i++) {
-            restrictions[i][1] = min(restrictions[i][1], restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0]);
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        for (int cost : costs) {
+            if (cost <= coins) {
+                coins -= cost;
+                count++;
+            } else {
+                break;
+            }
         }
-        
-        for (int i = restrictions.size() - 1; i > 0; i--) {
-            restrictions[i - 1][1] = min(restrictions[i - 1][1], restrictions[i][1] + restrictions[i][0] - restrictions[i - 1][0]);
-        }
-        
-        int ans = 0;
-        for (int i = 0; i < restrictions.size() - 1; i++) {
-            ans = max(ans, restrictions[i][1] + (restrictions[i + 1][0] - restrictions[i][0] - 1) / 2);
-        }
-        
-        return ans;
+        return count;
     }
 };
