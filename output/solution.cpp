134ba8ecaaf09a64f8479ebd0afc3e9307f5a137
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-23 05:05:03 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,13 +1,30 @@
+const int MOD = 1e9 + 7;
 class Solution {
 public:
-    int maxNumberOfBalloons(string text) {
-        // Count frequency of each character in the string
-        unordered_map<char, int> freq;
-        for (char c : text) {
-            freq[c]++;
+    int numberOfArrays(int n, int l, int r) {
+        vector<vector<long long>> dp(n, vector<long long>(2, 0));
+        dp[0][0] = r - l + 1;
+        dp[1][0] = r - l;
+        
+        // Compute dp values for n >= 2
+        for (int i = 1; i < n; i++) {
+            vector<vector<long long>> next(2, vector<long long>(2, 0));
+            for (int prevInc = 0; prevInc < 2; prevInc++) {
+                for (int prevDec = 0; prevDec < 2; prevDec++) {
+                    for (int j = l; j <= r; j++) {
+                        if (prevInc == 1 && prevDec == 0 && j < l + i) next[0][0] = (next[0][0] + dp[i-1][0]) % MOD; 
+                        if (prevInc == 0 && prevDec == 1 && j > r - i) next[1][1] = (next[1][1] + dp[i-1][1]) % MOD; 
+                        if (prevInc == 0 && prevDec == 0 && j > l + i - 1 && j < r - i + 1) next[0][1] = (next[0][1] + dp[i-1][0]) % MOD;
+                        if (prevInc == 1 && prevDec == 1 && j > l + i - 1 && j < r - i + 1) next[1][0] = (next[1][0] + dp[i-1][1]) % MOD;
+                    }
+                }
+            }
+            dp = next;
         }
         
-        // Calculate the maximum number of "balloon" instances
-        return min({freq['b'], freq['a'], freq['l'] / 2, freq['o'] / 2, freq['n']});
+        // Compute final answer
+        long long res = (dp[0][0] + dp[0][1] + dp[1][0] + dp[1][1]) % MOD;
+        
+        return res;
     }
 };
