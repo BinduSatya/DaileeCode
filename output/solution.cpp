4c823053167c89cc0c5a1e222500f5455a616970
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-25 05:10:06 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,31 +1,20 @@
-#include <vector>
-using namespace std;
-
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
-    int numberOfZigZagArrays(int n, int lower, int upper) {
-        long long dp[2][2] = {0}; // 0 for decreasing, 1 for increasing
-        for (int i = lower; i <= upper; i++) {
-            long long newDp[2][2] = {0};
-            if (!dp[0][0] && !dp[0][1] && !dp[1][0] && !dp[1][1]) {
-                newDp[0][0] = (upper - i < 1 ? 0 : upper - i);
-                newDp[0][1] = (i - lower < 1 ? 0 : i - lower);
-                newDp[1][0] = (upper - i < 1 ? 0 : upper - i);
-                newDp[1][1] = (i - lower < 1 ? 0 : i - lower);
-            } else {
-                newDp[0][0] = (dp[0][1] + dp[1][1]) % MOD;
-                newDp[0][1] = (dp[0][0] + dp[1][0]) % MOD;
-                newDp[1][0] = (dp[0][1] + dp[1][1]) % MOD;
-                newDp[1][1] = (dp[0][0] + dp[1][0]) % MOD;
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            int targetCount = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) {
+                    targetCount++;
+                }
+                if (targetCount > (j - i + 1) / 2) {
+                    count++;
+                }
             }
-            dp[0][0] = newDp[0][0];
-            dp[0][1] = newDp[0][1];
-            dp[1][0] = newDp[1][0];
-            dp[1][1] = newDp[1][1];
         }
-        long long ans = (dp[0][0] + dp[0][1] + dp[1][0] + dp[1][1]) % MOD;
-        return ans;
+        return count;
     }
 };
