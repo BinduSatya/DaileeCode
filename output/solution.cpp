0cdd20f884bcc7624769ad7d5864dd4449e26a57
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-26 05:11:50 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-        int count = 0;
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
+        long long count = 0;
         
         for (int i = 0; i < n; i++) {
-            int targetCount = 0;
+            int freq = 0;
             for (int j = i; j < n; j++) {
                 if (nums[j] == target) {
-                    targetCount++;
+                    freq++;
                 }
-                if (targetCount > (j - i + 1) / 2) {
+                if (2 * freq > j - i + 1) {
                     count++;
                 }
             }
         }
+        
         return count;
     }
 };
