575da378f0bf0736fcaf89d2e4a16879dad393a0
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-27 04:56:55 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        long long count = 0;
+    int maximumLength(vector<int>& nums) {
+        unordered_map<int, int> frequency;
+        int maxLength = 0;
         
-        for (int i = 0; i < n; i++) {
-            int freq = 0;
-            for (int j = i; j < n; j++) {
-                if (nums[j] == target) {
-                    freq++;
+        for (int num : nums) {
+            frequency[num]++;
+            int length = 1;
+            int k = num;
+            
+            while (k % 2 == 0) {
+                k /= 2;
+                if (frequency[k] > 0) {
+                    length++;
+                } else {
+                    break;
                 }
-                if (2 * freq > j - i + 1) {
-                    count++;
-                }
+            }
+            
+            if (k == 1) {
+                maxLength = max(maxLength, 2 * length - 1);
+            } else {
+                maxLength = max(maxLength, length);
             }
         }
         
-        return count;
+        return maxLength;
     }
 };
