14ca167c38c9db31469220c979fb5e9877c288bb
BinduSatya <BinduSatya@users.noreply.github.com>
2026-06-30 05:07:08 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int numOfStrings(vector<string>& patterns, string word) {
-        int count = 0;
-        for (const string& pattern : patterns) {
-            if (word.find(pattern) != string::npos) {
-                count++;
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int a = 0, b = 0, c = 0;
+            for (int j = i; j < n; j++) {
+                if (s[j] == 'a') a++;
+                else if (s[j] == 'b') b++;
+                else c++;
+                if (a > 0 && b > 0 && c > 0) res++;
             }
         }
-        return count;
+        return res;
     }
 };
