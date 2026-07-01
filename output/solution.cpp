17a692f91d8084004f734164432c796b9b0330e7
BinduSatya <BinduSatya@users.noreply.github.com>
2026-07-01 05:25:06 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,17 +1,59 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int n = s.size();
-        int res = 0;
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int maxSafeness = 0;
+        
+        // Calculate Manhattan distance to the nearest thief for each cell
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+        
         for (int i = 0; i < n; i++) {
-            int a = 0, b = 0, c = 0;
-            for (int j = i; j < n; j++) {
-                if (s[j] == 'a') a++;
-                else if (s[j] == 'b') b++;
-                else c++;
-                if (a > 0 && b > 0 && c > 0) res++;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
             }
         }
-        return res;
+        
+        vector<int> dirs = {-1, 0, 1, 0, -1};
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dirs[k];
+                int ny = y + dirs[k + 1];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && dist[nx][ny] > dist[x][y] + 1) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        
+        // Find the maximum safeness factor for all paths
+        set<int> safeness;
+        function<void(int, int, int)> dfs = [&](int x, int y, int currSafeness) {
+            if (x == n - 1 && y == n - 1) {
+                safeness.insert(currSafeness);
+                return;
+            }
+            
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dirs[k];
+                int ny = y + dirs[k + 1];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    dfs(nx, ny, min(currSafeness, dist[nx][ny]));
+                }
+            }
+        };
+        
+        dfs(0, 0, INT_MAX);
+        
+        return *safeness.rbegin();
     }
 };
