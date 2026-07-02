0db8694cc4a881f5f629405dc85332639a0b3a05
BinduSatya <BinduSatya@users.noreply.github.com>
2026-07-02 05:00:28 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,59 +1,41 @@
 class Solution {
 public:
-    int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int maxSafeness = 0;
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxHealth = m + n;
+        vector<vector<bool>> dp(m, vector<bool>(n, false));
+        dp[0][0] = true;
         
-        // Calculate Manhattan distance to the nearest thief for each cell
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        queue<pair<int, int>> q;
+        // Try to reach every cell in the first row
+        for (int i = 1; i < n; ++i) {
+            if (grid[0][i] == 0 && dp[0][i-1]) {
+                dp[0][i] = true;
+            } else if (grid[0][i] == 1 && dp[0][i-1] && health > 1) {
+                dp[0][i] = true;
+            }
+        }
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
+        // Try to reach every cell in the first column
+        for (int i = 1; i < m; ++i) {
+            if (grid[i][0] == 0 && dp[i-1][0]) {
+                dp[i][0] = true;
+            } else if (grid[i][0] == 1 && dp[i-1][0] && health > 1) {
+                dp[i][0] = true;
+            }
+        }
+        
+        // Fill the rest of the dp table
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                } else {
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]) && health > 1;
                 }
             }
         }
         
-        vector<int> dirs = {-1, 0, 1, 0, -1};
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dirs[k];
-                int ny = y + dirs[k + 1];
-                
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && dist[nx][ny] > dist[x][y] + 1) {
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-        
-        // Find the maximum safeness factor for all paths
-        set<int> safeness;
-        function<void(int, int, int)> dfs = [&](int x, int y, int currSafeness) {
-            if (x == n - 1 && y == n - 1) {
-                safeness.insert(currSafeness);
-                return;
-            }
-            
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dirs[k];
-                int ny = y + dirs[k + 1];
-                
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                    dfs(nx, ny, min(currSafeness, dist[nx][ny]));
-                }
-            }
-        };
-        
-        dfs(0, 0, INT_MAX);
-        
-        return *safeness.rbegin();
+        return dp[m-1][n-1];
     }
 };
