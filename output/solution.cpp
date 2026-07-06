8df0da9af1b3607840d6a83f4df27dfe803e0fb2
BinduSatya <BinduSatya@users.noreply.github.com>
2026-07-06 05:15:00 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
-    vector<int> pathsWithMaxScore(vector<string>& board) {
-        int n = board.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, 0)));
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+        });
         
-        dp[n - 1][n - 1][0] = board[n - 1][n - 1] == 'S' ? 0 : board[n - 1][n - 1] - '0';
-        dp[n - 1][n - 1][1] = 1;
+        int count = 0;
+        int right = 0;
         
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                if (i == n - 1 && j == n - 1) continue;
-                if (board[i][j] == 'X' || board[i][j] == 'E') {
-                    dp[i][j][1] = 0;
-                    if (board[i][j] == 'E') {
-                        dp[i][j][0] = 0;
-                    }
-                    continue;
-                }
-                
-                vector<int> maxScoreAndCount(2, 0);
-                if (i + 1 < n && dp[i + 1][j][0] > maxScoreAndCount[0]) {
-                    maxScoreAndCount[0] = dp[i + 1][j][0];
-                    maxScoreAndCount[1] = dp[i + 1][j][1];
-                }
-                else if (i + 1 < n && dp[i + 1][j][0] == maxScoreAndCount[0]) {
-                    maxScoreAndCount[1] = (maxScoreAndCount[1] + dp[i + 1][j][1]) % 1000000007;
-                }
-                
-                if (j + 1 < n && dp[i][j + 1][0] > maxScoreAndCount[0]) {
-                    maxScoreAndCount[0] = dp[i][j + 1][0];
-                    maxScoreAndCount[1] = dp[i][j + 1][1];
-                }
-                else if (j + 1 < n && dp[i][j + 1][0] == maxScoreAndCount[0]) {
-                    maxScoreAndCount[1] = (maxScoreAndCount[1] + dp[i][j + 1][1]) % 1000000007;
-                }
-                
-                if (i + 1 < n && j + 1 < n && dp[i + 1][j + 1][0] > maxScoreAndCount[0]) {
-                    maxScoreAndCount[0] = dp[i + 1][j + 1][0];
-                    maxScoreAndCount[1] = dp[i + 1][j + 1][1];
-                }
-                else if (i + 1 < n && j + 1 < n && dp[i + 1][j + 1][0] == maxScoreAndCount[0]) {
-                    maxScoreAndCount[1] = (maxScoreAndCount[1] + dp[i + 1][j + 1][1]) % 1000000007;
-                }
-                
-                dp[i][j][0] = maxScoreAndCount[0] + board[i][j] - '0';
-                dp[i][j][1] = maxScoreAndCount[1];
+        for (const auto& interval : intervals) {
+            if (interval[1] > right) {
+                count++;
+                right = interval[1];
             }
         }
         
-        return {dp[0][0][0], dp[0][0][1]};
+        return count;
     }
 };
