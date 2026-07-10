4b7d39a5f32ff2d5f81cd03345c5d536f915db7f
BinduSatya <BinduSatya@users.noreply.github.com>
2026-07-10 04:56:09 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,45 +1,51 @@
-class UnionFind {
-public:
-    vector<int> parent;
-    UnionFind(int n) {
-        parent.resize(n);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    void unionNodes(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            parent[rootX] = rootY;
-        }
-    }
-};
-
 class Solution {
 public:
-    vector<bool> areConnected(int n, vector<int>& nums, int threshold, vector<vector<int>>& queries) {
-        UnionFind uf(n);
+    vector<int> shortestPath(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<unordered_map<int, int>> graph(n);
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (abs(nums[i] - nums[j]) <= threshold) {
-                    uf.unionNodes(i, j);
+            for (int j = 0; j < n; j++) {
+                if (i != j && abs(nums[i] - nums[j]) <= maxDiff) {
+                    graph[i][j] = 1;
                 }
             }
         }
         
-        vector<bool> result;
+        vector<int> results;
         for (auto& query : queries) {
-            result.push_back(uf.find(query[0]) == uf.find(query[1]));
+            int start = query[0], end = query[1];
+            if (start == end) {
+                results.push_back(0);
+                continue;
+            }
+            
+            queue<pair<int, int>> q;
+            q.push({start, 0});
+            vector<bool> visited(n, false);
+            visited[start] = true;
+            
+            bool found = false;
+            while (!q.empty()) {
+                auto [node, dist] = q.front();
+                q.pop();
+                
+                for (auto& neighbor : graph[node]) {
+                    if (!visited[neighbor.first]) {
+                        visited[neighbor.first] = true;
+                        if (neighbor.first == end) {
+                            results.push_back(dist + 1);
+                            found = true;
+                            break;
+                        }
+                        q.push({neighbor.first, dist + 1});
+                    }
+                }
+                
+                if (found) break;
+            }
+            
+            if (!found) results.push_back(-1);
         }
-        return result;
+        
+        return results;
     }
 };
