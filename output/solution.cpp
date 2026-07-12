fbe09b4d820486733e0ae84a234032686f3aee74
BinduSatya <BinduSatya@users.noreply.github.com>
2026-07-12 04:29:25 +0000

    deploy: 54ddbc9425b30eea01f7e8c30fba92b1000d006f

--- output/solution.cpp
+++ output/solution.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n);
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-        }
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> sorted = arr;
+        sort(sorted.begin(), sorted.end());
+        map<int, int> ranks;
+        int rank = 1;
         
-        vector<bool> visited(n, false);
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                vector<int> component;
-                dfs(graph, i, visited, component);
-                
-                bool isComplete = true;
-                for (int u : component) {
-                    for (int v : component) {
-                        if (u != v && find(graph[u].begin(), graph[u].end(), v) == graph[u].end()) {
-                            isComplete = false;
-                            break;
-                        }
-                    }
-                    if (!isComplete) break;
-                }
-                
-                if (isComplete) count++;
+        for (int num : sorted) {
+            if (ranks.find(num) == ranks.end()) {
+                ranks[num] = rank++;
             }
         }
         
-        return count;
-    }
-    
-    void dfs(vector<vector<int>>& graph, int node, vector<bool>& visited, vector<int>& component) {
-        visited[node] = true;
-        component.push_back(node);
-        
-        for (int neighbor : graph[node]) {
-            if (!visited[neighbor]) {
-                dfs(graph, neighbor, visited, component);
-            }
+        vector<int> result;
+        for (int num : arr) {
+            result.push_back(ranks[num]);
         }
+        
+        return result;
     }
 };
